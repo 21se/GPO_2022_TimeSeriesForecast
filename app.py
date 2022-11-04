092cec0b2ab2d50f1842a04e91ecacfcbd983b81
21se <21se90210@gmail.com>
2022-11-04 11:27:50 +0700

    web

---
+++ app.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template, request, flash, url_for, Response, jsonify
+import db
+
+app = Flask(__name__)
+app.secret_key = '3228'
+
+
+@app.route('/')
+def main_window():
+    return render_template('Main_window.html')
+
+
+@app.route('/about')
+def about():
+    model = request.args.get('model')
+    template = 'Data_about_model_window.html' if not model else f'{model}.html'
+
+    return render_template(template)
+
+
+@app.route('/data')
+def data():
+    data_type = request.args.get('type')
+    if not data_type in ('train', 'test', 'raw', 'prediction'):
+        return render_template('Kust_Choice.html')
+
+    db_data = db.get_data(data_type)
+    if len(db_data) == 0:
+        flash('Нет данных')
+        return render_template('Kust_Choice.html')
+
+    page_data = []
+    if data_type in ('train', 'test'):
+        for row in db_data:
+            page_data.append(
+                {
+                    'Исходные данные': {'text': f'Куст {row["well_id"]} ({row["parameter"]})',
+                                        'url': f'data_graph?data_type=raw&data_id={row["raw_data_id"]}'},
+                    'Данные скорректированы': {'text': 'Да' if row['corrections'] else 'Нет'},
+                    'Величина нормализации': {'text': row['normalization_value']},
+                    'url': f'/data_graph?data_type={data_type}&data_id={row["id"]}'
+                }
+            )
+    elif data_type == 'raw':
+        for row in db_data:
+            page_data.append(
+                {
+                    'Куст': {'text': row['well_id']},
+                    'Параметр': {'text': row['parameter']},
+                    'url': f'/data_graph?data_type={data_type}&data_id={row["id"]}'
+                }
+            )
+    elif data_type == 'prediction':
+        for row in db_data:
+            page_data.append(
+                {
+                    'Тестовые данные':
+                        {'text': f'Куст {row["well_id"]} ({row["parameter"]})',
+                         'url': f'data_graph?data_type=test&data_id={row["test_data_id"]}',
+                         'title': f'Данные{" не " if not row["test_data_corrections"] else " "}скорректированы, '
+                                  f'величина нормализации: {row["normalization_value"]}'},
+                    'Модель': {'text': row["network"]},
+                    'url': f'/data_graph?data_type={data_type}&data_id={row["id"]}'
+                }
+            )
+    if page_data:
+        return render_template('data_choice.html', page_data=page_data)
+
+    return render_template('Kust_Choice.html')
+
+
+@app.route('/data_graph')
+def get_data_graph():
+    if 'data_type' in request.args and 'data_id' in request.args:
+        return render_template('Data_graph_window.html',
+                               data_type=request.args['data_type'], data_id=request.args['data_id'])
+    return render_template('Data_graph_window.html')
+
+
+@app.route('/data_values/<data_type>/<int:data_id>')
+def get_data_values(data_type, data_id):
+    if data_type not in ('train', 'test', 'raw', 'prediction'):
+        return 'Wrong data_type', 400
+
+    if
+
+    data_values = db.get_data_values(data_type, data_id)
+
+    return jsonify(data_values)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ db.py
@@ -0,0 +1,103 @@
+import psycopg2
+from psycopg2.extras import DictCursor
+
+DB_NAME = 'rpas'
+DB_USER = 'postgres'
+DB_PWD = '1234'
+DB_HOST = 'localhost'
+
+
+def get_db_handles(dbname, user, password, host):
+    connection = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
+    cursor = connection.cursor(cursor_factory=DictCursor)
+
+    return connection, cursor
+
+
+def get_data(data_type):
+    conn, cursor = get_db_handles(DB_NAME, DB_USER, DB_PWD, DB_HOST)
+
+    query = ''
+    if data_type == 'raw':
+        query = get_raw_query()
+    elif data_type == 'prediction':
+        query = get_prediction_query()
+    elif data_type in ('train', 'test'):
+        query = get_train_test_query(data_type)
+
+    cursor.execute(query)
+
+    data = cursor.fetchall()
+
+    return data
+
+
+def get_data_values(data_type, data_id):
+    conn, cursor = get_db_handles(DB_NAME, DB_USER, DB_PWD, DB_HOST)
+
+    query = f"""
+        SELECT 
+            D.DATA_VALUE
+        FROM {data_type}_DATA_VALUES AS D
+        WHERE    
+            D.{data_type}_DATA_ID = {data_id}      
+        """
+    cursor.execute(query)
+
+    data = cursor.fetchall()
+    flatten_data = sum(data, [])
+
+    return flatten_data
+
+
+def get_raw_query():
+    return f"""
+        SELECT 
+            RD.WELL_ID,
+            RD.PARAMETER,
+            RD.ID
+        FROM RAW_DATA AS RD
+        ORDER BY
+            RD.ID DESC         
+        """
+
+
+def get_prediction_query():
+    return f"""
+        SELECT 
+            RD.WELL_ID,
+            RD.PARAMETER,
+            PD.TEST_DATA_ID,
+            TD.CORRECTIONS AS TEST_DATA_CORRECTIONS,
+            TD.NORMALIZATION_VALUE,
+            M.NETWORK,
+            PD.MODEL_ID,
+            PD.ID
+        FROM PREDICTION_DATA AS PD
+            INNER JOIN TEST_DATA AS TD
+                ON PD.TEST_DATA_ID = TD.ID
+            INNER JOIN RAW_DATA AS RD 
+                ON TD.RAW_DATA_ID = RD.ID
+            INNER JOIN MODELS AS M
+                ON PD.MODEL_ID = M.ID
+        ORDER BY
+            PD.ID DESC     
+    """
+
+
+def get_train_test_query(data_type):
+    return f"""
+        SELECT 
+            RD.WELL_ID,
+            RD.PARAMETER,
+            D.RAW_DATA_ID,
+            D.CORRECTIONS,
+            D.NORMALIZATION_VALUE,
+            D.ID
+        FROM {data_type}_DATA AS D
+            INNER JOIN RAW_DATA AS RD 
+                ON D.RAW_DATA_ID = RD.ID
+        ORDER BY
+            D.ID DESC         
+        """
+
