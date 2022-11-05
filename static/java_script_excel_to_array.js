d8f39af8d7f3b3fa7ccc2885ada346b7f2b6f2f0
21se <21se90210@gmail.com>
2022-11-05 20:25:32 +0700

    redesign by Vladanus

--- static/java_script_excel_to_array.js
+++ static/java_script_excel_to_array.js
@@ -10,20 +10,19 @@ excel_file.addEventListener('change', (event) => {
 
         return false;
     }
-		var texts = document.querySelectorAll('p');
-		/*for(var i = 0; i<texts.length; i++)
-				{
-						texts[i].
-				}*/
+    var texts = document.querySelectorAll('p');
+    /*for(var i = 0; i<texts.length; i++)
+            {
+                    texts[i].
+            }*/
 
-		document.getElementById('first_column').style.display = 'none';
-		document.getElementById('second_column').style.display = 'none';
-		document.getElementById('third_column').style.display = 'none';
-		document.getElementById('fourth_column').style.display = 'none';
+    document.getElementById('footer').style.visibility = 'hidden';
+
     var reader = new FileReader();
 
     reader.readAsArrayBuffer(event.target.files[0]);
-    reader.onload = function(event){
+
+    reader.onload = function(event) {
         var data = new Uint8Array(reader.result);
         var work_book = XLSX.read(data, {type:'array'});
         var sheet_name = work_book.SheetNames;
@@ -31,86 +30,84 @@ excel_file.addEventListener('change', (event) => {
 
         if(sheet_data.length > 0)
         {
-						var DoData = new Array(sheet_data.length);
-						var PosleData = new Array(sheet_data.length);
-						var TempData = new Array(sheet_data.length);
-						var RasxData = new Array(sheet_data.length);
+            var DoData = new Array(sheet_data.length);
+            var PosleData = new Array(sheet_data.length);
+            var TempData = new Array(sheet_data.length);
+            var RasxData = new Array(sheet_data.length);
+
             for(var row = 3; row < sheet_data.length; row++)
             {
 								//NewData[row-3] = new Array(1);
                 for(var cell = 0; cell < sheet_data[row].length; cell++)
                 {
-										if(sheet_data[row][cell] != '#N/A' || sheet_data[row][cell] != '0')
-										{
-												switch (cell)
-												{
-													case 1:
-													{
-														DoData[row-3] = sheet_data[row-1][cell];
-														break;
-													}
-													case 2:
-													{
-														PosleData[row-3] = sheet_data[row-1][cell];
-														break;
-													}
-													case 3:
-													{
-														TempData[row-3] = sheet_data[row-1][cell];
-														break;
-													}
-													case 4:
-													{
-														RasxData[row-3] = sheet_data[row-1][cell];
-														break;
-													}
-												}
-									  }
+                    if(sheet_data[row][cell] != '#N/A' || sheet_data[row][cell] != '0')
+                    {
+                        switch (cell)
+                        {
+                            case 1:
+                            {
+                                DoData[row-3] = sheet_data[row-1][cell];
+                                break;
+                            }
+                            case 2:
+                            {
+                                PosleData[row-3] = sheet_data[row-1][cell];
+                                break;
+                            }
+                            case 3:
+                            {
+                                TempData[row-3] = sheet_data[row-1][cell];
+                                break;
+                            }
+                            case 4:
+                            {
+                                RasxData[row-3] = sheet_data[row-1][cell];
+                                break;
+                            }
+                        }
+                    }
                 }
             }
         }
 
-				for(var i =0; i<texts.length; i++)
-				{
-					texts[i].addEventListener('click',
-					function(e)
-					{
-						 switch (e.target.getAttribute('id'))
-								 {
-									 	case 'first_column':
-										{
-											localStorage.setItem("Do", JSON.stringify(DoData));
-											break;
-										}
-										case 'second_column':
-										{
-											localStorage.setItem("Do", JSON.stringify(PosleData));
-											break;
-										}
-										case 'third_column':
-										{
-											localStorage.setItem("Do", JSON.stringify(TempData));
-											break;
-										}
-										case 'fourth_column':
-										{
-											localStorage.setItem("Do", JSON.stringify(RasxData));
-											break;
-										}
-								 }
-								 window.open('data_graph','','Toolbar=1,Location=0,Directories=0,Status=0,Menubar=0,Scrollbars=0,Resizable=Yes,Width=1200,Height=600, Left = 350, Top = 200, position: relative;');
+        for(var i =0; i<texts.length; i++)
+        {
+            texts[i].addEventListener('click',
+            function(e)
+            {
+                 switch (e.target.getAttribute('id'))
+                     {
+                        case 'first_column':
+                        {
+                            localStorage.setItem("Do", JSON.stringify(DoData));
+                            break;
+                        }
+                        case 'second_column':
+                        {
+                            localStorage.setItem("Do", JSON.stringify(PosleData));
+                            break;
+                        }
+                        case 'third_column':
+                        {
+                            localStorage.setItem("Do", JSON.stringify(TempData));
+                            break;
+                        }
+                        case 'fourth_column':
+                        {
+                            localStorage.setItem("Do", JSON.stringify(RasxData));
+                            break;
+                        }
+                     }
+                     window.open('data_graph','','Toolbar=1,Location=0,Directories=0,Status=0,Menubar=0,Scrollbars=0,Resizable=Yes,Width=1200,Height=600, Left = 350, Top = 200, position: relative;');
 
-					})
-				}
+            })
+        }
 
-				/*for(var i = 0; i<texts.length; i++)
-				{
-						texts[i].style.display = '';
-				}*/
-				document.getElementById('first_column').style.display = '';
-				document.getElementById('second_column').style.display = '';
-				document.getElementById('third_column').style.display = '';
-				document.getElementById('fourth_column').style.display = '';
+        /*for(var i = 0; i<texts.length; i++)
+        {
+                texts[i].style.display = '';
+        }*/
+        document.getElementById('footer').style.visibility = 'visible';
     }
 
 });
